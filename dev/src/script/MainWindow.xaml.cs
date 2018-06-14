251e8acdc63f8fb8d1f88accb25601fe4037a545
bull313 <blladi313@gmail.com>
2018-06-13 18:10:29 -0700

    Created colorless text editor with function-less menu

---
+++ dev/src/script/MainWindow.xaml.cs
@@ -0,0 +1,36 @@
+﻿using System.Windows;
+
+namespace ide
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        /*
+         *  ---------------- TEXT EDITOR STYLE HANDLERS ----------------
+        */
+
+        
+
+        /*
+        *   ---------------- / TEXT EDITOR STYLE HANDLERS ----------------
+        */
+
+        /*
+         *  ---------------- MENU BAR CLICK HANDLERS ----------------
+        */
+
+        private void File_Exit_Click(object sender, RoutedEventArgs e)
+        {
+            /* Exit the program */
+            System.Environment.Exit(0); // TODO: ask for save
+        }
+
+        /*
+         *  ---------------- / MENU BAR CLICK HANDLERS ----------------
+        */
+    }
+}
