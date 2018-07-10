90502a4cf651767bac9dc458566954ec835e367b
Benjamin Ladick <ben.ladick@garmin.com>
2018-07-10 08:57:28 -0700

    Created token class and test (test passes)
    Requirement: 2.6

--- dev/src/lang/Compiler.cs
+++ dev/src/lang/Compiler.cs
@@ -6,30 +6,31 @@ using System.Threading.Tasks;
 
 namespace compiler
 {
-    class Compiler
-    {
-
-    }
-
     enum TokenType
     {
-
-    }
+        /* Basic Lexicons */
+        NEWLINE, LBRACKET, RBRACKET, LETTER, DIGIT, UNDERSCORE, DOLLARSIGN, BANG, POUND, QUOTE, LPAREN, RPAREN, LBRACE, RBRACE, AMPERSAND, ASTERISK, SEMICOLON, DOT, APOS, COMMA, EQUAL, GREATER, LESS, PIPE, SLASH,
+        /* Compound Lexicons */
+        BREAK, ID, ALNUM, SIGN, NOTE, STRING, LARROW, RARROW, LARRORLARGE, RARROWLARGE, NUMBER,
+        /* Tier 1 Keywords */
+        ACCOMPANY, NAME, AUTHOR, COAUTHORS, TITLE, KEY, TIME, TEMPO, OCTAVE, PATTERN, CHORD, IS,
+        /* Tier 2 Keywords */
+        COMMON, CUT,
+        /* Tier 3 Keywords */
+        REPEAT, LAYER,
+        /* Unknown Token Type (that means there's a syntax error) */
+        UNKNOWN
+    };
 
     class Token
     {
-        public string content { get { return content; } set { } }
-        public TokenType type { get { return type; } set { } }
+        public readonly string Content;
+        public readonly TokenType Type;
 
         public Token(string content, TokenType type)
         {
-            this.content = content;
-            this.type = type;
+            this.Content = content;
+            this.Type = type;
         }
-    }
-
-    class LexicalAnalyzer
-    {
-
     }
 }
--- dev/src/lang/CompilerTest.cs
+++ dev/src/lang/CompilerTest.cs
@@ -11,7 +11,102 @@ namespace compilerTest
     {
         static void Main(string[] args)
         {
+            int passCount = 0;
             Console.WriteLine("Compiler Tests:");
+            Test[] testList = { new Test__Token() };
+
+            foreach (Test test in testList)
+            {
+                Console.WriteLine("Begin " + test.GetTestName() + " Test:\n");
+                test.RunTests();
+                Console.WriteLine(test.GetResults());
+                if (test.Passes())
+                    ++passCount;
+            }
+        }
+    }
+
+    abstract class Test
+    {
+        private string resultNotes = "";
+        protected string testName = "Unnamed";
+        protected int comparisons = 0, failures = 0;
+
+        public abstract void RunTests();
+
+        public string GetTestName()
+        {
+            return testName;
+        }
+
+        public bool Passes()
+        {
+            return comparisons > 0 && failures == 0;
+        }
+
+        public string GetResults()
+        {
+            return resultNotes + "\nOverall Results:\n"
+                + "Total Comparisons: " + comparisons + "\n"
+                + "Total Failures: " + failures + "\n"
+                + "Test Result: " + (Passes() ? "PASS" : "FAIL") + "\n";
+        }
+
+        public void VerifyEqual(dynamic one, dynamic two)
+        {
+            ++comparisons;
+            if (one == two)
+            {
+                resultNotes += "\"" + one + "\"" + " = " + "\"" + two + "\"" + ": Comparison #" + comparisons + " passes\n";
+            }
+            else
+            {
+                resultNotes += "Failure at comparison #" + comparisons + ": " + one + " == " + two + " is false!\n";
+                ++failures;
+            }
+        }
+
+        public void VerifyEqualObj(object one, object two)
+        {
+            ++comparisons;
+            if (one.Equals(two))
+            {
+                resultNotes += one + " = " + two + ": Comparison #" + comparisons + " passes\n";
+            }
+            else
+            {
+                resultNotes += "Failure at comparison #" + comparisons + ": " + one + " == " + two + " is false!\n";
+                ++failures;
+            }
+        }
+    }
+
+    class Test__Token : Test
+    {
+        public Test__Token()
+        {
+            testName = "Token";
+        }
+
+        public void TestConstructor()
+        {
+            Token testToken1 = new Token("coauthors", TokenType.COAUTHORS);
+            Token testToken2 = new Token("beniscool", TokenType.ID);
+            Token testToken3 = new Token("@", TokenType.UNKNOWN);
+
+            VerifyEqual(testToken1.Content, "coauthors");
+            VerifyEqualObj(testToken1.Type, TokenType.COAUTHORS);
+
+            VerifyEqual(testToken2.Content, "beniscool");
+            VerifyEqualObj(testToken2.Type, TokenType.ID);
+
+            VerifyEqual(testToken3.Content, "@");
+            VerifyEqualObj(testToken3.Type, TokenType.UNKNOWN);
+        }
+
+        public override void RunTests()
+        {
+            TestConstructor();
         }
     }
 }
