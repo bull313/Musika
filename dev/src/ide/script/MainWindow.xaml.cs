c8437648a4b900d567d9f88ef4b6c7c10bd58cca
Benjamin Ladick <ben.ladick@garmin.com>
2018-07-18 17:50:29 -0700

    Trying to add syntax highlighting
    Requirement: 2.1

--- dev/src/ide/script/MainWindow.xaml.cs
+++ dev/src/ide/script/MainWindow.xaml.cs
@@ -1,24 +1,70 @@
-﻿using System.Windows;
+﻿using System;
+using System.Collections.Generic;
+using System.Text.RegularExpressions;
+using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Documents;
+using System.Windows.Input;
 using System.Windows.Media;
 
 namespace ide
 {
     public partial class MainWindow : Window
     {
+        private static Color DEFAULT_COLOR = Colors.Black; /* Set the default text color to black */
+        private static Color TIER_1_KEYWORD_COLOR = Colors.Red;
+
+        private Paragraph body;
+        private Dictionary<string, Color> syntaxHighlightDict;
+
         public MainWindow()
         {
             InitializeComponent();
+            ResetEditor();
+
+            /* Populate Text Highlight Dictionary */
+            syntaxHighlightDict = new Dictionary<string, Color>();
+            syntaxHighlightDict.Add("accompany",    TIER_1_KEYWORD_COLOR);
+            syntaxHighlightDict.Add("name",         TIER_1_KEYWORD_COLOR);
+            syntaxHighlightDict.Add("author",       TIER_1_KEYWORD_COLOR);
+            syntaxHighlightDict.Add("coauthors",    TIER_1_KEYWORD_COLOR);
+            syntaxHighlightDict.Add("title",        TIER_1_KEYWORD_COLOR);
+            syntaxHighlightDict.Add("key",          TIER_1_KEYWORD_COLOR);
+            syntaxHighlightDict.Add("time",         TIER_1_KEYWORD_COLOR);
+            syntaxHighlightDict.Add("tempo",        TIER_1_KEYWORD_COLOR);
+            syntaxHighlightDict.Add("octave",       TIER_1_KEYWORD_COLOR);
+            syntaxHighlightDict.Add("pattern",      TIER_1_KEYWORD_COLOR);
+            syntaxHighlightDict.Add("chord",        TIER_1_KEYWORD_COLOR);
+            syntaxHighlightDict.Add("is",           TIER_1_KEYWORD_COLOR);
         }
+
+        /*
+         *  ---------------- HELPER FUNCTIONS ----------------
+        */
+
+        private string GetText()
+        {
+            return new TextRange(Editor.Document.ContentStart, Editor.Document.ContentEnd).Text;
+        }
+
+        private void ResetEditor() /* Make a blank new file */
+        {
+            Editor.Document.Blocks.Clear();
+            body = new Paragraph(); /* Insert a parent paragraph into the document */
+            Editor.Document.Blocks.Add(body);
+        }
+
+        /*
+         *  ---------------- / HELPER FUNCTIONS --------------
+        */
+
 
         /*
          *  ---------------- TEXT EDITOR STYLE HANDLERS ----------------
         */
 
-        private void Editor_KeyDown(object sender, RoutedEventArgs e)
+        private void Editor_TextChanged(object sender, TextChangedEventArgs e)
         {
-            string editorText = new TextRange(Editor.Document.ContentStart, Editor.Document.ContentEnd).Text;
 
         }
 
@@ -39,8 +85,7 @@ namespace ide
 
         private void File_New_Click(object sender, RoutedEventArgs e)
         {
-            /* Make a blank new file */
-            Editor.Document.Blocks.Clear();
+            ResetEditor();
         }
 
         /*
